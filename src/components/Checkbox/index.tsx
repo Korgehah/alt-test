e42f64ec0180e65480aa995e69dcecbfcdfbb02d
Korgehah <korgehah@gmail.com>
2022-03-24 20:04:39 +0300

    ADD: Checkbox

---
+++ src/components/Checkbox/index.tsx
@@ -0,0 +1,38 @@
+import React from 'react';
+
+interface CheckboxProps {
+  children: React.ReactNode;
+  smallText?: boolean;
+}
+
+const Checkbox = ({ children, smallText }: CheckboxProps) => {
+  return (
+    <label className='checkbox'>
+      <input className='checkbox__field' type='checkbox' />
+      <span
+        className={`checkbox__inner ${
+          smallText ? 'checkbox__inner_small' : ''
+        }`}
+      >
+        <svg
+          className='checkbox__symbol'
+          width='10'
+          height='8'
+          viewBox='0 0 10 8'
+          fill='none'
+          xmlns='http://www.w3.org/2000/svg'
+        >
+          <path
+            d='M8.92162 1.39307L4.06868 6.09895L1.86279 3.95991'
+            stroke='white'
+            strokeWidth='2'
+            strokeLinecap='round'
+            strokeLinejoin='round'
+          />
+        </svg>
+        {children}
+      </span>
+    </label>
+  );
+};
+export default Checkbox;
